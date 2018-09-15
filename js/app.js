6cc4130952189ebe0bf2169b00ebbf6a0a2cfca9
Sarah Stensberg <ladystensberg@gmail.com>
2018-09-15 13:52:21 -0700

    set up wireframing

---
+++ js/app.js
@@ -0,0 +1,11 @@
+// USER STORIES:
+    // User should see empty tic-tac-toe board on page load
+
+    // User can click any non-played cell in the grid to place
+    // either a X or an O
+
+    // User can reset the game with a reset button, which will 
+    // remove all Xs and Os from board and allow user to start
+    // the game again
+
+    
