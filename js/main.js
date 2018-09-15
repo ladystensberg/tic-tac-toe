e734ae02abf3991c75bcef4758a73c9565b2a938
Sarah Stensberg <ladystensberg@gmail.com>
2018-09-15 15:03:39 -0700

    initialized main.js file and added game icon images

--- js/main.js
+++ js/main.js
@@ -8,4 +8,38 @@
     // remove all Xs and Os from board and allow user to start
     // the game again
 
-    
+
+/* ----------------------------------------------- */
+
+
+
+/*----- constants -----*/
+
+
+
+
+/*----- app's state (variables) -----*/
+
+
+
+
+/*----- cached element references -----*/
+
+var body = document.body;
+var resetButton = document.querySelector("button");
+var gameBoard = document.getElementById("game-board");
+
+
+/*----- event listeners -----*/
+
+gameBoard.addEventListener("click", function(event) {
+    console.log(event.target.id);
+    event.target.innerHTML = "<h5>" + event.target.id + "</h5>";
+})
+
+
+/*----- functions -----*/
+
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("DOM loaded");
+});
